2b503840344a59dae336ac3e80ae7bf6de0bcc21
Shahir-Awlad <shahir.thebomb@gmail.com>
2024-11-18 20:17:17 +0600

    Made minor changes to the zakat tab to make it more presentable

--- src/Shahir/zakat.java
+++ src/Shahir/zakat.java
@@ -1,19 +1,19 @@
 ZakwalletListView.setItems(observableList);
-        ZakwalletListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-        if (newValue != null) {
-String walName;
-ResultSet tot_Amount;
-walName = newValue.getName();
-                nameField.setText(newValue.getName());
+ZakwalletListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+    if (newValue != null) {
+        String walName;
+        ResultSet tot_Amount;
+        walName = newValue.getName();
+        nameField.setText(newValue.getName());
         descField.setText(newValue.getDescription());
         try (Connection connection = Makeconnection.makeconnection()) {
-PreparedStatement preparedStatement = connection.prepareStatement("select current_balance from wallet_balance_view where wallet_name = ?");
-                    preparedStatement.setString(1, walName);
-tot_Amount = preparedStatement.executeQuery();
-int val = tot_Amount.getInt("current_balance");
-                    zakField.setText(String.valueOf(val * .025));
+        PreparedStatement preparedStatement = connection.prepareStatement("select current_balance from wallet_balance_view where wallet_name = ?");
+        preparedStatement.setString(1, walName);
+        tot_Amount = preparedStatement.executeQuery();
+        int val = tot_Amount.getInt("current_balance");
+        zakField.setText(String.valueOf(val * .025));
         } catch (SQLException e) {
-        throw new RuntimeException(e);
-                }
-                        }
-                        });
+            throw new RuntimeException(e);
+        }
+    }
+});
