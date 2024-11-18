bcc8d9660626d1c20dab910ea0153bf42d948395
Shahir-Awlad <shahir.thebomb@gmail.com>
2024-11-18 20:11:02 +0600

    Started work on zakat tab

--- src/Shahir/zakat.java
+++ src/Shahir/zakat.java
@@ -1,1 +1,9 @@
 ZakwalletListView.setItems(observableList);
+ZakwalletListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+    if (newValue != null) {
+    String walName;
+    ResultSet tot_Amount;
+    walName = newValue.getName();
+                nameField.setText(newValue.getName());
+        descField.setText(newValue.getDescription());
+        try (Connection connection = Makeconnection.makeconnection()) {
